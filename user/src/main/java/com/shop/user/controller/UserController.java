dcf45d547c60e4b99fc87905b0fc7300690c75aa
CroiXan <xan.mark.vi@gmail.com>
2024-11-17 01:40:27 -0300

    fix tabla usuarios

--- user/src/main/java/com/shop/user/controller/UserController.java
+++ user/src/main/java/com/shop/user/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException.UnprocessableEntity;
 
 import com.shop.user.exception.ResourceNotFoundException;
 import com.shop.user.exception.UserNotFoundException;
